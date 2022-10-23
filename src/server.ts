1691f2517b7728d10ba76da33606cc3dfbd53a4b
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-10-23 17:55:32 -0300

    feat: server init boilerplate

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { Server, createServer } from "http";
+
+let server: Server;
+
+function startServer(port: number) {
+  const app = express();
+
+  app.use(express.json({ limit: "5mb" }));
+
+  server = createServer(app);
+
+  server.listen(port);
+}
+
+function stopServer(error?: Error) {
+  if (error) {
+    try {
+      server.close();
+      process.exit(1);
+    } catch (e: unknown) {
+      process.exit(1);
+    }
+  }
+  process.exit(0);
+}
+
+export { startServer, stopServer };
