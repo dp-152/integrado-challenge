7291ac42623121a371a358c0c23935880e2343aa
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-10-23 22:13:47 -0300

    feat: updated unauthorized error with enum for reason

--- src/util/errors/unauthorizedError.ts
+++ src/util/errors/unauthorizedError.ts
@@ -1,9 +1,11 @@
+import EUnauthorizedReason from "../Enums/EUnauthorizedReason";
 import HttpError from "./httpError";
 
 export default class UnauthorizedError extends HttpError {
-  constructor() {
+  constructor(reason: EUnauthorizedReason) {
     super("You do not have access to the requested resource");
     this.statusCode = 401;
+    this.data = reason;
   }
 
   public toJSON() {
