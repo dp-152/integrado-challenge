de134a8fd1531cfe00786c849bd62a519be9c199
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-10-24 21:51:56 -0300

    feat: seeding the database with import data

--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,25 @@ import router from "./api/routes";
 import errorHandler from "./middlewares/errorHandler";
 import requestID from "./middlewares/requestID";
 import { MONGODB_CONNECTION_STRING } from "./config/settings";
+import { Seeder } from "mongo-seeding";
+import path from "path";
+import populateImports from "./util/populateImports";
 
 let server: Server;
 
 async function startServer(port: number) {
   const app = express();
 
-  await mongooseConnect(MONGODB_CONNECTION_STRING);
+  await populateImports();
+
+  const seeder = new Seeder({ database: MONGODB_CONNECTION_STRING });
+  const collections = seeder.readCollectionsFromPath(
+    path.join(__dirname, "./data/import")
+  );
+  await Promise.all([
+    seeder.import(collections),
+    await mongooseConnect(MONGODB_CONNECTION_STRING),
+  ]);
 
   app.use(express.json({ limit: "5mb" }));
   app.use(requestID);
