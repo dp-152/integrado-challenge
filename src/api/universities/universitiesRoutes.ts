b5ae6be2f9fe0acb92e6b6293ba3310c735b0cb9
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-10-25 02:48:24 -0300

    feat: added controller to router

--- src/api/universities/universitiesRoutes.ts
+++ src/api/universities/universitiesRoutes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import getUniversitiesListController from "./getUniversitiesListController";
 import getUniversityByIDController from "./getUniversityByIDController";
 
 const universitiesRouter = Router(); // eslint-disable-line new-cap
 
 universitiesRouter.get("/:id", getUniversityByIDController);
+universitiesRouter.get("/", getUniversitiesListController);
 
 export default universitiesRouter;
 
