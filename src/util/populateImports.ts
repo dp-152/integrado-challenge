62cca2467f2ba17b2209ae2e15d2b7cc6f893c39
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-10-24 21:58:22 -0300

    fix: using correct paths for import directory

--- src/util/populateImports.ts
+++ src/util/populateImports.ts
@@ -5,10 +5,12 @@ import TUniversity from "../data/types/universityType";
 
 export default async function populateImports() {
   try {
-    await fs.access("../data/import/universities");
+    await fs.access(path.join(__dirname, "../data/import/universities"));
     return;
   } catch {
-    await fs.mkdir("../data/import/universities", { recursive: true });
+    await fs.mkdir(path.join(__dirname, "../data/import/universities"), {
+      recursive: true,
+    });
 
     const countries = [
       "argentina",
@@ -24,12 +26,15 @@ export default async function populateImports() {
     await Promise.all(
       countries.map(async country => {
         const { data: response } = await axios.get<TUniversity[]>(
-          "http://universities.hipolabs.com/search/",
+          "http://universities.hipolabs.com/search",
           { params: { country } }
         );
 
         await fs.writeFile(
-          path.join(`../data/import/universities/universities-${country}.json`),
+          path.join(
+            __dirname,
+            `../data/import/universities/universities-${country}.json`
+          ),
           JSON.stringify(response)
         );
       })
