35f64c8a808cb7a9ee4b27ab7dc2950f1a9d96c6
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-10-24 23:35:51 -0300

    feat: added validator for request

---
+++ src/application/university/query/getUniversityByIDValidator.ts
@@ -0,0 +1,17 @@
+import { isValidObjectId } from "mongoose";
+import TValidationResult from "../../../util/types/validationResult";
+import IGetUniversityByIDRequestDTO from "./getUniversityByIDRequestDTO";
+
+export default function validateGetUniversityByID(
+  request: IGetUniversityByIDRequestDTO
+): TValidationResult {
+  let isValid = true;
+  const errorList: string[] = [];
+
+  if (!isValidObjectId(request.id)) {
+    isValid = false;
+    errorList.push("ID is not a valid ObjectId");
+  }
+
+  return [isValid, errorList];
+}
