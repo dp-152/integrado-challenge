ddcaeb41deb77402b3c06abc0e899261e02d467b
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2022-10-24 23:41:00 -0300

    feat: using router for university

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import statusRouter from "./status/statusRoutes";
 import defaultRouter from "./default/defaultRoutes";
+import universitiesRouter from "./universities/universitiesRoutes";
 
 const router = Router(); // eslint-disable-line new-cap
 
+router.use("/university", universitiesRouter);
 router.use("/status", statusRouter);
 router.use("(.*)", defaultRouter);
 
